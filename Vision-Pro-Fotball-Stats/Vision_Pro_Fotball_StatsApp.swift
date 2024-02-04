53672507df47170703bd421fe124ef9640b23521
ioanaydgn <mertaydogn0@gmail.com>
2024-02-04 14:12:49 +0200

    Projekt erstellt

---
+++ Vision-Pro-Fotball-Stats/Vision_Pro_Fotball_StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vision_Pro_Fotball_StatsApp.swift
+//  Vision-Pro-Fotball-Stats
+//
+//  Created by Mert Aydoğan on 04.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Vision_Pro_Fotball_StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
